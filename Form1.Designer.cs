3ac698b15ba42dcaf2c28114961c7670e7a395eb
Administrator <Administrator@AS-PC>
2024-02-17 10:25:45 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace tst
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Targetword = new System.Windows.Forms.Label();
+            wordamount = new System.Windows.Forms.NumericUpDown();
+            examplebutton = new System.Windows.Forms.Button();
+            trueOrfalse = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            Titlelabel = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)wordamount).BeginInit();
+            SuspendLayout();
+            // 
+            // Targetword
+            // 
+            Targetword.AutoSize = true;
+            Targetword.Location = new System.Drawing.Point(12, 38);
+            Targetword.Name = "Targetword";
+            Targetword.Size = new System.Drawing.Size(16, 15);
+            Targetword.TabIndex = 0;
+            Targetword.Text = "! !";
+            // 
+            // wordamount
+            // 
+            wordamount.Location = new System.Drawing.Point(108, 7);
+            wordamount.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            wordamount.Name = "wordamount";
+            wordamount.Size = new System.Drawing.Size(33, 23);
+            wordamount.TabIndex = 1;
+            wordamount.ThousandsSeparator = true;
+            wordamount.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // examplebutton
+            // 
+            examplebutton.Location = new System.Drawing.Point(147, 7);
+            examplebutton.Name = "examplebutton";
+            examplebutton.Size = new System.Drawing.Size(133, 23);
+            examplebutton.TabIndex = 2;
+            examplebutton.Text = "Generate words";
+            examplebutton.UseVisualStyleBackColor = true;
+            examplebutton.Click += examplebutton_Click;
+            // 
+            // trueOrfalse
+            // 
+            trueOrfalse.AutoSize = true;
+            trueOrfalse.Location = new System.Drawing.Point(12, 128);
+            trueOrfalse.Name = "trueOrfalse";
+            trueOrfalse.Size = new System.Drawing.Size(10, 15);
+            trueOrfalse.TabIndex = 4;
+            trueOrfalse.Text = "!";
+            // 
+            // textBox1
+            // 
+            textBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            textBox1.Location = new System.Drawing.Point(12, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(340, 23);
+            textBox1.TabIndex = 5;
+            textBox1.WordWrap = false;
+            textBox1.KeyDown += Form1_KeyDown;
+            // 
+            // Titlelabel
+            // 
+            Titlelabel.AutoSize = true;
+            Titlelabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            Titlelabel.Location = new System.Drawing.Point(12, 9);
+            Titlelabel.Name = "Titlelabel";
+            Titlelabel.Size = new System.Drawing.Size(81, 15);
+            Titlelabel.TabIndex = 6;
+            Titlelabel.Text = "Target words";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(12, 103);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(178, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Press ENTER to check answer...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(377, 154);
+            Controls.Add(label1);
+            Controls.Add(Titlelabel);
+            Controls.Add(textBox1);
+            Controls.Add(trueOrfalse);
+            Controls.Add(examplebutton);
+            Controls.Add(wordamount);
+            Controls.Add(Targetword);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "tst";
+            ((System.ComponentModel.ISupportInitialize)wordamount).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Targetword;
+        private System.Windows.Forms.NumericUpDown wordamount;
+        private System.Windows.Forms.Button examplebutton;
+        private System.Windows.Forms.Label trueOrfalse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Titlelabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Security.AccessControl;
+using System.Transactions;
+using System.Windows.Forms;
+namespace tst
+{
+    public partial class Form1 : Form
+    {
+        public string[] wordlist = { "civilian", "mathematics", "medium", "formulate", "preparation", "society", "illusion", "corruption", "memory", "momentum", "ancestor", "opposite", "appreciate", "ecstasy", "photography", "remember", "financial", "repetition", "cooperation", "agreement", "embarrassment", "solution", "agenda", "legislation", "nonremittal", "transaction", "objective", "undermine", "handicap", "anxiety" };
+        System.Diagnostics.Stopwatch sw = new();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string GetRandomString()
+        {
+            Random r = new Random();
+            string word = "";
+            for (int x = 0; x < wordamount.Value; x++)
+            {
+                word += wordlist[r.Next(0, wordlist.Length)];
+                word += " ";
+                if (r.Next(0, 10) < 4)
+                {
+                    string symbols = "$?!.,-:;'";
+                    word += symbols[r.Next(0, symbols.Length)];
+                    word += " ";
+                }
+            }
+            return word.TrimEnd(' ');
+        }
+        private void examplebutton_Click(object sender, EventArgs e)
+        {
+            startTime();
+        }
+
+        private void startTime()
+        {
+            sw.Stop();
+            sw.Reset();
+            textBox1.Clear();
+            trueOrfalse.Text = "";
+            textBox1.Focus();
+            Targetword.Text = GetRandomString();
+            sw.Start();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            checktextbox(e);
+        }
+        private void checktextbox(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == Targetword.Text)
+                {
+                    sw.Stop();
+                    trueOrfalse.Text = $"Correct! {sw.Elapsed.TotalSeconds}s";
+                    trueOrfalse.ForeColor = System.Drawing.Color.Green;
+                }
+                else if (textBox1.Text != Targetword.Text)
+                {
+                    trueOrfalse.Text = "Wrong!";
+                    trueOrfalse.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+    }
+}
